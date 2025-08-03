af749d136272096c29a8e1430a74a2324e96915b
ashutosh sharma <ashusharmads.18@gmail.com>
2025-08-03 15:53:04 +0530

    final push

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,16 +5,15 @@ import connectDb from './lib/db.js';
 import authRoutes from './routes/authRoutes.js';
 import cookieParser from 'cookie-parser';
 import postRoutes from './routes/postRoutes.js';
-import path from 'path'
+import path from 'path';
 
 
 dotenv.config();
 
 const app = express();
-const PORT=process.env.PORT || 5001
+const PORT = process.env.PORT || 5001
 
 const __dirname=path.resolve()
-
 
 app.use(cors({
   origin: 'http://localhost:5173',  // your frontend URL
@@ -30,9 +29,18 @@ app.use('/api/posts', postRoutes);
 
 
 
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    
+   app.get("*",(req,res)=>{
+      res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+   })
+}
+
 
 // MongoDB Connect & Server Start
-app.listen(PORT,()=>{
-    console.log(`PORT is running at ${PORT} PORT `)
-    connectDb()
+app.listen(PORT, () => {
+  console.log(`PORT is running at ${PORT} PORT `)
+  connectDb()
 })
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,27 @@
+import axios from 'axios'
+
+const BASE_URL=import.meta.env.MODE==="development"? "http://localhost:5001/api":"/api"
+
+const axiosInstance=axios.create({
+    baseURL:BASE_URL,
+    withCredentials:true,
+})
+
+export default axiosInstance;
+
+/*
 import axios from 'axios';
 
-const Base_URL=import.meta.env.Mode==="development"? "http://localhost:5001/api" :"/api"
+const Base_URL=import.meta.env.MODE==="development"? "http://localhost:5001/api" :"/api"
+
+const axiosInstance = axios.create({
+  baseURL: Base_URL,
+  withCredentials: true,
+});
+
+export default axiosInstance;
+
+
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:5001/api',
@@ -8,3 +29,4 @@ const axiosInstance = axios.create({
 });
 
 export default axiosInstance;
+*/
